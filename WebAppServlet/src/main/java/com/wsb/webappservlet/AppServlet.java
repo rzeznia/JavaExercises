4f512f144ee7fc0984d0b744e9d37d7bd7edec69
rzeznia <mrzeznix@mrzeznix-MOBL1.ger.corp.intel.com>
2018-01-20 14:14:33 +0100

    Added WebApplication TOdo: Char coding (PL chars not working)

--- WebAppServlet/src/main/java/com/wsb/webappservlet/AppServlet.java
+++ WebAppServlet/src/main/java/com/wsb/webappservlet/AppServlet.java
@@ -16,26 +16,19 @@ public class AppServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-         
-        // read form fields
         String name = request.getParameter("name");
         String surname = request.getParameter("surname");
          
         System.out.println("name: " + name);
         System.out.println("sur: " + surname);
- 
-        // do some processing here...
-         
-        // get response writer
+        
         PrintWriter writer = response.getWriter();
          
-        // build HTML code
         String htmlRespone = "<html>";
         htmlRespone += "<h2>Your username is: " + name + "<br/>";      
         htmlRespone += "Your password is: " + surname + "</h2>";    
         htmlRespone += "</html>";
-         
-        // return response
+
         writer.println(htmlRespone);        
     }
 }
